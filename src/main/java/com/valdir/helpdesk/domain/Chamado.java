a918ded69161d06e31c77ad03919e1ef6770e0a3
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2023-12-13 13:17:09 -0300

    anotações JPA para classes de domínio

--- src/main/java/com/valdir/helpdesk/domain/Chamado.java
+++ src/main/java/com/valdir/helpdesk/domain/Chamado.java
@@ -1,24 +1,45 @@
 package com.valdir.helpdesk.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.valdir.helpdesk.domain.enums.Prioridade;
 import com.valdir.helpdesk.domain.enums.Status;
+import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataAbertura = LocalDate.now();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
+
     private Prioridade prioridade;
+
     private Status status;
+
     private String titulo;
+
     private String observacoes;
 
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
     private Tecnico tecnico;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
     public Chamado(Integer id, Prioridade prioridade, Status status, String titulo, String observacoes, Tecnico tecnico, Cliente cliente) {
+        super();
         this.id = id;
         this.prioridade = prioridade;
         this.status = status;
@@ -26,6 +47,10 @@ public class Chamado {
         this.observacoes = observacoes;
         this.tecnico = tecnico;
         this.cliente = cliente;
+    }
+
+    public Chamado() {
+        super();
     }
 
     public Integer getId() {
--- src/main/java/com/valdir/helpdesk/domain/Tecnico.java
+++ src/main/java/com/valdir/helpdesk/domain/Tecnico.java
@@ -1,18 +1,27 @@
 package com.valdir.helpdesk.domain;
 
+import com.valdir.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa {
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Long id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
