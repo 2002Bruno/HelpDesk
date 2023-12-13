5a2d1d174851287d23eaa98ec04cd627ff1480ca
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2023-12-13 19:03:14 -0300

    Protegendo aplicação contra cerialização.

--- src/main/java/com/valdir/helpdesk/domain/Cliente.java
+++ src/main/java/com/valdir/helpdesk/domain/Cliente.java
@@ -1,5 +1,6 @@
 package com.valdir.helpdesk.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.valdir.helpdesk.domain.enums.Perfil;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -11,6 +12,7 @@ import java.util.List;
 public class Cliente extends Pessoa {
     private static final long serialVersionUID = 1L;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
--- src/main/java/com/valdir/helpdesk/domain/Tecnico.java
+++ src/main/java/com/valdir/helpdesk/domain/Tecnico.java
@@ -1,5 +1,6 @@
 package com.valdir.helpdesk.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.valdir.helpdesk.domain.enums.Perfil;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -11,6 +12,7 @@ import java.util.List;
 public class Tecnico extends Pessoa {
     private static final long serialVersionUID = 1L;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
