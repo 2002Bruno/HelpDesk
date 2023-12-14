7be9a8c3b131ced76b1c2a49aa3ee93eb39c2d71
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2023-12-14 10:50:43 -0300

    [BACKEND] - Exceção para CPF e E-mail já cadastrados no sistema.

--- src/main/java/com/valdir/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/valdir/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -17,4 +17,12 @@ public class ResourceExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
+
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Violação de dados", ex.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 }
--- src/main/java/com/valdir/helpdesk/services/TecnicoService.java
+++ src/main/java/com/valdir/helpdesk/services/TecnicoService.java
@@ -1,8 +1,11 @@
 package com.valdir.helpdesk.services;
 
+import com.valdir.helpdesk.domain.Pessoa;
 import com.valdir.helpdesk.domain.Tecnico;
 import com.valdir.helpdesk.dtos.TecnicoDTO;
+import com.valdir.helpdesk.repository.PessoaRepository;
 import com.valdir.helpdesk.repository.TecnicoRepository;
+import com.valdir.helpdesk.resources.exceptions.DataIntegrityViolationException;
 import com.valdir.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,9 @@ public class TecnicoService {
 
     @Autowired
     private TecnicoRepository tecnicoRepository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
 
     public Tecnico findById(Integer id) {
@@ -31,7 +37,21 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO tecnicoDTO) {
         tecnicoDTO.setId(null);
+        validaPorCpfEEmail(tecnicoDTO);
         Tecnico newObj = new Tecnico(tecnicoDTO);
         return tecnicoRepository.save(newObj);
+    }
+
+    private void validaPorCpfEEmail(TecnicoDTO tecnicoDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(tecnicoDTO.getCpf());
+
+        if (obj.isPresent() && obj.get().getId() != tecnicoDTO.getId()) {
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+        }
+
+        obj = pessoaRepository.findByEmail(tecnicoDTO.getEmail());
+        if (obj.isPresent() && obj.get().getId() != tecnicoDTO.getId()) {
+            throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+        }
     }
 }
