580cc58f7ad44414ed6ca7b3aad5949885391205
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2024-01-24 14:42:57 -0300

    [FRONTEND] - Criação e estilização do componente home.

--- helpdesk-front/helpfront/src/app/components/home/home.component.spec.ts
+++ helpdesk-front/helpfront/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      schemas: [
+        [CUSTOM_ELEMENTS_SCHEMA]
+      ]
     })
     .compileComponents();
   });
