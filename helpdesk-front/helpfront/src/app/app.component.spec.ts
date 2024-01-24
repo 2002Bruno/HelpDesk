bcf8e5d72d2a8e1f2a6e83fe7bf9e9dfe7164ca2
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2024-01-24 11:50:07 -0300

    [FRONTEND] - Importação dos módulos para aplicação e testes unitários.

--- helpdesk-front/helpfront/src/app/app.component.spec.ts
+++ helpdesk-front/helpfront/src/app/app.component.spec.ts
@@ -2,11 +2,57 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+//Para trabalhar com formulários no Angular
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+//Para realizar requisições HTTP
+import { HttpClientModule } from "@angular/common/http";
+
+//Imports para componentes do Angular Material
+import { MatButtonModule } from "@angular/material/button";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSelectModule } from "@angular/material/select";
+import { MatTableModule } from "@angular/material/table";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        //Forms
+        FormsModule,
+        ReactiveFormsModule,
+        //Requisições HTTP
+        HttpClientModule,
+        // Angular Material
+        MatFormFieldModule,
+        MatPaginatorModule,
+        MatSnackBarModule,
+        MatCheckboxModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatTableModule,
+        MatRadioModule,
+        MatInputModule,
+        MatIconModule,
+        MatListModule,
+        MatCardModule
       ],
       declarations: [
         AppComponent
--- helpdesk-front/helpfront/src/app/app.module.ts
+++ helpdesk-front/helpfront/src/app/app.module.ts
@@ -4,6 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSelectModule } from "@angular/material/select";
+import { MatTableModule } from "@angular/material/table";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
@@ -12,7 +29,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    //Forms
+    FormsModule,
+    ReactiveFormsModule,
+    //Requisições HTTP
+    HttpClientModule,
+    // Angular Material
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatTableModule,
+    MatRadioModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
