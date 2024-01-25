8eaa38bcdc9f2839230b490e69d58bc05cdc70b1
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2024-01-25 09:44:53 -0300

    [BA] - Adição da configuração do cors configuration para url localhost:4200.

--- helpDesk/src/main/java/com/valdir/helpdesk/config/SecurityConfig.java
+++ helpDesk/src/main/java/com/valdir/helpdesk/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
