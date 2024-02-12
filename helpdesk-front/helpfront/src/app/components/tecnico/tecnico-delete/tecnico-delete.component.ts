8616e17191b002c4e557fbee88a70ef6f9650d3d
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2024-02-12 11:26:50 -0300

    [FRONTEND] - Alteração do tecnico delete, (implementação do formulário reativo), e remoção do antigo model que era utilizado para controle do formulário.

--- helpdesk-front/helpfront/src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ helpdesk-front/helpfront/src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { TecnicoModel } from "../../../models/tecnico";
 import { ActivatedRoute, Router } from "@angular/router";
 import { TecnicoService } from "../../../services/tecnico.service";
 import { ToastrService } from "ngx-toastr";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-tecnico-delete',
@@ -11,39 +11,61 @@ import { ToastrService } from "ngx-toastr";
 })
 export class TecnicoDeleteComponent implements OnInit {
 
-  tecnico: TecnicoModel = {
-    id: '',
-    nome: '',
-    cpf: '',
-    email: '',
-    senha: '',
-    perfis: [],
-    dataCriacao: '',
-  }
+  tecnicoForm: FormGroup;
+  tecnicoId: any;
 
   constructor(
     private router: Router,
     private tecnicoService: TecnicoService,
     private toast: ToastrService,
     private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
   ) {
   }
 
   ngOnInit(): void {
-    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.tecnicoId = this.route.snapshot.paramMap.get('id');
+    this.inicializarForm();
+
 
     this.findById();
   }
 
+  inicializarForm() {
+    this.tecnicoForm = new FormGroup({
+
+      id: new FormControl(this.tecnicoId, Validators.required),
+      nome: new FormControl(null, [Validators.minLength(3), Validators.required]),
+      cpf: new FormControl(null, [Validators.required, Validators.maxLength(11)]),
+      email: new FormControl(null),
+      senha: new FormControl(null, [Validators.minLength(3), Validators.required]),
+    });
+  }
+
+  preencherForm(tecnico) {
+    this.tecnicoForm = this.formBuilder.group({
+      id: [tecnico.id],
+      nome: [tecnico.nome],
+      cpf: [tecnico.cpf],
+      email: [tecnico.email],
+      senha: [tecnico.senha],
+    });
+
+    this.tecnicoForm.controls['nome'].disable();
+    this.tecnicoForm.controls['cpf'].disable();
+    this.tecnicoForm.controls['email'].disable();
+    this.tecnicoForm.controls['senha'].disable();
+  }
+
   findById(): void {
-    this.tecnicoService.findById(this.tecnico.id).subscribe(response => {
-      response.perfis = [];
-      this.tecnico = response;
+    this.tecnicoService.findById(this.tecnicoId).subscribe(response => {
+
+      this.preencherForm(response);
     });
   }
 
   delete(): void {
-    this.tecnicoService.delete(this.tecnico.id).subscribe(response => {
+    this.tecnicoService.delete(this.tecnicoForm.value.id).subscribe(response => {
       this.toast.success('Técnico deletado com sucesso', 'Exclusão');
       this.router.navigate(['/tecnicos']);
     }, error => {
