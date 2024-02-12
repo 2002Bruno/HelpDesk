362723ed4f494669a4bd58751ed30a7dd1ea369c
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2024-02-12 11:32:43 -0300

    [FRONTEND] - Alteração do cliente create, (implementação do formulário reativo), e remoção do antigo model que era utilizado para controle do formulário.

--- helpdesk-front/helpfront/src/app/components/cliente/tecnico-create/cliente-create.component.ts
+++ helpdesk-front/helpfront/src/app/components/cliente/tecnico-create/cliente-create.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import { FormControl, Validators } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ToastrService } from "ngx-toastr";
-import { ClienteModel } from "../../../models/cliente";
 import { ClienteService } from "../../../services/cliente.service";
 
 @Component({
@@ -12,20 +11,7 @@ import { ClienteService } from "../../../services/cliente.service";
 })
 export class ClienteCreateComponent implements OnInit {
 
-  cliente: ClienteModel = {
-    id: '',
-    nome: '',
-    cpf: '',
-    email: '',
-    senha: '',
-    perfis: [],
-    dataCriacao: '',
-  }
-
-  nome: FormControl = new FormControl(null, Validators.minLength(3));
-  cpf: FormControl = new FormControl(null, [Validators.required, Validators.maxLength(11)]);
-  email: FormControl = new FormControl(null, Validators.email);
-  senha: FormControl = new FormControl(null, Validators.minLength(3));
+  clienteForm: FormGroup;
 
   constructor(
     private router: Router,
@@ -35,10 +21,20 @@ export class ClienteCreateComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.inicializarForm();
+  }
+
+  inicializarForm() {
+    this.clienteForm = new FormGroup({
+      nome: new FormControl(null, [Validators.minLength(3), Validators.required]),
+      cpf: new FormControl(null, [Validators.required, Validators.maxLength(11)]),
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      senha: new FormControl(null, [Validators.minLength(3), Validators.required]),
+    });
   }
 
   onSubmit(): void {
-    this.clienteService.create(this.cliente).subscribe(response => {
+    this.clienteService.create(this.clienteForm.value).subscribe(response => {
       this.toast.success('Cliente cadastrado com sucesso', 'Cadastro');
       this.router.navigate(['/clientes']);
     }, error => {
@@ -52,18 +48,8 @@ export class ClienteCreateComponent implements OnInit {
     });
   }
 
-  addPerfil(perfil: any): void {
-    this.cliente.perfis.push(perfil);
-
-    if (this.cliente.perfis.includes(perfil)) {
-      this.cliente.perfis.splice(this.cliente.perfis.indexOf(perfil), 1);
-    } else {
-      this.cliente.perfis.push(perfil);
-    }
-  }
-
   validarCampos() {
-    return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid;
+    return this.clienteForm.valid;
   }
 
   voltar() {
